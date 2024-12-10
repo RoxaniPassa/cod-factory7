582c4abbae96062a04006de1cf2b82d4a697adde
Roxy <roxanipassa12@gmail.com>
2024-12-11 00:05:43 +0200

    initial commit

---
+++ src/gr/aeub/cf/ch1/HelloWorld.java
@@ -0,0 +1,19 @@
+package gr.aeub.cf.ch1;
+
+/**
+ * Prints Hello Coding Factory in standard output (console).
+ * @author roxan javadoc
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //Prints Hello Coding Factory
+        /*
+        * Prints in standard output
+        * "Hello Coding Factory"
+         */
+        System.out.println("Hello Coding World!"); //Prints Hello Coding Factory
+        System.out.print("Hello World");
+        System.out.println();
+    }
+}
